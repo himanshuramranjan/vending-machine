0ebdf69f5be468a6f2cecdef61c5d9476937e26d
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-03-16 22:16:26 +0530

    Vending Machine interface defined

--- src/states/VendingMachineStates.java
+++ src/states/VendingMachineStates.java
@@ -3,7 +3,6 @@ package states;
 public interface VendingMachineStates {
     void selectProducts(Product product);
     void insertNote(Note note);
-
     void dispenseProduct();
     void returnChange();
 }
