bcc7927de7a9641b7efdf5fe2993cb29b7d58c38
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-03-17 23:18:50 +0530

    refactored the select product signature

--- src/states/VendingMachineState.java
+++ src/states/VendingMachineState.java
@@ -4,7 +4,7 @@ import enums.Note;
 import models.Product;
 
 public interface VendingMachineState {
-    void selectProducts(Product product);
+    void selectProducts(int productCode);
     void insertNote(Note note);
     void dispenseProduct();
     void returnChange();
